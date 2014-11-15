badf09b91e97ee03d956d8e51bbf1eeec6087b7f
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2014-11-15 14:07:19 +0100

    Resource handles Http Malformed

--- app/org/restfulscala/playmachine/resource/Resource.scala
+++ app/org/restfulscala/playmachine/resource/Resource.scala
@@ -8,20 +8,27 @@ trait Resource extends Controller {
   // TODO use proper HttpMethod case class
   def allowedMethods : Set[String] = Set("GET", "HEAD")
 
+  def isMalformed(request : Request[_], pathParams: Seq[PathParam]) : Boolean = false
+
   def handleRequest(pathParams: Seq[PathParam]): EssentialAction = Action { request =>
   	???
 
   }
 
-  def handleAllowedMethods(request : Request[_]): EssentialAction = {
+  def handleAllowedMethods(request : Request[_], pathParams: Seq[PathParam]): EssentialAction = {
   	allowedMethods.contains(request.method) match {
-      case true => handleBadRequest(request)
+      case true => handleBadRequest(request, pathParams)
       case false => Action {request => Results.MethodNotAllowed}
     }
   }
 
-  def handleBadRequest(request : Request[_]): EssentialAction = ???
+  def handleBadRequest(request : Request[_], pathParams: Seq[PathParam]): EssentialAction = {
+  	(!isMalformed(request, pathParams)) match {
+      case true => handleUnauthorized(request, pathParams)
+      case false => Action {request => Results.BadRequest}
+    }
+  }
 
-  def handleUnauthorized(request : Request[_]): EssentialAction = ???
+  def handleUnauthorized(request : Request[_], pathParams: Seq[PathParam]): EssentialAction = ???
 
 }
