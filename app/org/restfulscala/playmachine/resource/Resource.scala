7fee9ac2fbe051ff1f3bbdf1def6f356489110f4
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2014-11-15 14:37:26 +0100

    Resource handles Http Not Implemented

--- app/org/restfulscala/playmachine/resource/Resource.scala
+++ app/org/restfulscala/playmachine/resource/Resource.scala
@@ -1,6 +1,6 @@
 package org.restfulscala.playmachine.resource
 
-import play.api.mvc.{Action, EssentialAction, Controller, Results, Request, Result}
+import play.api.mvc.{Action, EssentialAction, Controller, Results, Request, Result, Headers}
 import org.restfulscala.playmachine.resource._
 
 trait Resource extends Controller {
@@ -14,6 +14,8 @@ trait Resource extends Controller {
 
   def isForbidden(request : Request[_], pathParams: Seq[PathParam]) : Boolean = false
 
+  def isImplemented(headers : Headers) : Boolean = true
+
   def handleRequest(pathParams: Seq[PathParam]): EssentialAction = Action { request =>
   	// Bootstrap decision tree
   	handleAllowedMethods(request, pathParams)
@@ -21,32 +23,39 @@ trait Resource extends Controller {
 
   def handleAllowedMethods(request : Request[_], pathParams: Seq[PathParam]): Result = {
   	allowedMethods.contains(request.method) match {
-      case true => handleBadRequest(request, pathParams)
+      case true  => handleBadRequest(request, pathParams)
       case false => Results.MethodNotAllowed
     }
   }
 
   def handleBadRequest(request : Request[_], pathParams: Seq[PathParam]): Result = {
-  	(!isMalformed(request, pathParams)) match {
-      case true => handleUnauthorized(request, pathParams)
+  	!isMalformed(request, pathParams) match {
+      case true  => handleUnauthorized(request, pathParams)
       case false => Results.BadRequest
     }
   }
 
   def handleUnauthorized(request : Request[_], pathParams: Seq[PathParam]): Result = {
-  	(!isAuthorized(request, pathParams)) match {
-      case true => handleForbidden(request, pathParams)
+  	isAuthorized(request, pathParams) match {
+      case true  => handleForbidden(request, pathParams)
       case false => Results.Unauthorized
     }
   }
 
   def handleForbidden(request : Request[_], pathParams: Seq[PathParam]): Result = {
-  	(!isForbidden(request, pathParams)) match {
-      case true => handleNotImplemented(request, pathParams)
+  	!isForbidden(request, pathParams) match {
+      case true  => handleNotImplemented(request, pathParams)
       case false => Results.Forbidden
     }
   }
 
-  def handleNotImplemented(request : Request[_], pathParams: Seq[PathParam]): Result = ???
+  def handleNotImplemented(request : Request[_], pathParams: Seq[PathParam]): Result = {
+  	isImplemented(request.headers) match {
+      case true  => handleUnsupportedMediaType(request, pathParams)
+      case false => Results.NotImplemented
+    }
+  }
+
+  def handleUnsupportedMediaType(request : Request[_], pathParams: Seq[PathParam]): Result = ???
 
 }
