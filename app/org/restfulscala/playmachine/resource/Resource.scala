17c24c32d1996121c661bbf9dbdcbd987c24adf1
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2014-11-15 13:51:38 +0100

    Resource handles Http Allowed-Methods

--- app/org/restfulscala/playmachine/resource/Resource.scala
+++ app/org/restfulscala/playmachine/resource/Resource.scala
@@ -1,13 +1,27 @@
 package org.restfulscala.playmachine.resource
 
-import play.api.mvc.{Action, EssentialAction, Controller}
+import play.api.mvc.{Action, EssentialAction, Controller, Results, Request}
+import org.restfulscala.playmachine.resource._
 
 trait Resource extends Controller {
 
-  def allowedMethods = Seq()
+  // TODO use proper HttpMethod case class
+  def allowedMethods : Set[String] = Set("GET", "HEAD")
 
   def handleRequest(pathParams: Seq[PathParam]): EssentialAction = Action { request =>
-    Ok("foo")
+  	???
+
   }
+
+  def handleAllowedMethods(request : Request[_]): EssentialAction = {
+  	allowedMethods.contains(request.method) match {
+      case true => handleBadRequest(request)
+      case false => Action {request => Results.MethodNotAllowed}
+    }
+  }
+
+  def handleBadRequest(request : Request[_]): EssentialAction = ???
+
+  def handleUnauthorized(request : Request[_]): EssentialAction = ???
 
 }
